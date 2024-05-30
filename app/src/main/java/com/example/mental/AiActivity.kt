79fbf3a3ca329cf9e0349964eddcd6bac5c42248
deeeasert <y26885099@gmail.com>
2024-05-30 18:41:10 +0900

    바텀 시트 추가(수정필) / 앱 아이콘 업데이트 / AI 페이지 업데이트

--- app/src/main/java/com/example/mental/AiActivity.kt
+++ app/src/main/java/com/example/mental/AiActivity.kt
@@ -1,6 +1,7 @@
 package com.example.mental
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -19,4 +20,6 @@ class AiActivity : AppCompatActivity() {
 //            insets
 //        }
     }
+
+    fun btn_Click(view: View) {}
 }
